2c5335111fc9bf5e4e9bfbd644df2b4e8cefd875
Xorus <jdpepi@gmail.com>
2020-06-05 17:58:16 +0200

    Updates

---
+++ precache-manifest.a8e88d57d9d1518adcb09e56190db996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6bfa9643a15c476933ca7ac8de02ba",
+    "url": "/xivtrans/index.html"
+  },
+  {
+    "revision": "bc44edb6498c2ab3d62c",
+    "url": "/xivtrans/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33d26151b8c1679c9465",
+    "url": "/xivtrans/static/js/2.1de9d5db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xivtrans/static/js/2.1de9d5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc44edb6498c2ab3d62c",
+    "url": "/xivtrans/static/js/main.89bca3c3.chunk.js"
+  },
+  {
+    "revision": "5d53c8d24a1564f73297",
+    "url": "/xivtrans/static/js/runtime-main.36ea888e.js"
+  }
+]);
